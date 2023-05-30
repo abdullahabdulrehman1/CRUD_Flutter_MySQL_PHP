0ac695a83729a632fe247a216ea60175f2e83195
abdullahabdulrehman1 <abdullahabdurrehman72@gmail.com>
2023-05-31 03:23:31 +0500

    Update

--- lib/model/usermodel.dart
+++ lib/model/usermodel.dart
@@ -1,20 +1,27 @@
-// import 'package:flutter/foundation.dart';
-
 class UserModel {
   String? id;
   String? name;
   String? email;
+
   UserModel({this.email, this.id, this.name});
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
     return UserModel(
-        email: json['email'] as String,
-       
-        name: json['name'] as String);
+      email: json['email'] as String,
+      name: json['name'] as String,
+    );
   }
+
   Map<String, dynamic> toJsonAdd() {
     return {
-     
+      "email": email,
+      "name": name,
+    };
+  }
+
+  Map<String, dynamic> toJsonUpdate() {
+    return {
+      "id": id,
       "email": email,
       "name": name,
     };
